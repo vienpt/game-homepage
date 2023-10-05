6e66e23d8555c5687b3a412cbc7889d37e3353d9
vienpham <viendev@outlook.com>
2023-10-06 00:20:17 +0700

    fix: remove card stats

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import AppHeader from './components/AppHeader.jsx'
 import AppHero from './components/AppHero.jsx'
 import AppCompany from './components/AppCompany.jsx'
 import AppStats from './components/AppStats.jsx'
-import CardStat from "./components/CardStat.jsx";
 
 function App() {
   return (
