ddf63cf9588e8fce50d8783aba73072eeb50694e
vienpham <viendev@outlook.com>
2023-10-06 10:22:23 +0700

    fix: update app stat size

--- src/components/AppStats.jsx
+++ src/components/AppStats.jsx
@@ -4,15 +4,15 @@ export default function AppStats() {
   return (
     <section id="stats" className="mx-auto max-w-[1024px] py-12 grid md:grid-cols-3 grid-rows-1 gap-6 place-content-evenly">
       <div className="relative">
-        <img src={'/svg/stats-1.svg'} alt="stats-1" />
+        <img className="w-[90%] md:w-auto mx-auto" src={'/svg/stats-1.svg'} alt="stats-1" />
         <ButtonSecondary className="btn-play-now leading-8 md:text-xl lg:text-2xl" text="Play Now" />
       </div>
       <div className="relative">
-        <img src={'/svg/stats-2.svg'} alt="stats-2" />
+        <img className="w-[90%] md:w-auto mx-auto" src={'/svg/stats-2.svg'} alt="stats-2" />
         <ButtonSecondary className="btn-play-now leading-8 md:text-xl lg:text-2xl" text="Play Now" />
       </div>
       <div className="relative">
-        <img src={'/svg/stats-3.svg'} alt="stats-3" />
+        <img className="w-[90%] md:w-auto mx-auto" src={'/svg/stats-3.svg'} alt="stats-3" />
         <ButtonSecondary className="btn-play-now leading-8 md:text-xl lg:text-2xl" text="Play Now" />
       </div>
     </section>
