9257885d6f8a95b9232004da499cea9ceec23c84
vienpham <viendev@outlook.com>
2023-10-07 00:00:49 +0700

    fix: section title img top

--- src/components/SectionTitle.jsx
+++ src/components/SectionTitle.jsx
@@ -17,7 +17,7 @@ function SectionTitle({ hasImage, text, className, children }) {
         alt="hero-icon"
         width="80"
         height="90"
-        className="absolute md:w-auto md:h-auto w-[64px] h-[74px] top-[-6rem] left-[42%] md:left-[45%] lg:left-[47%] -translate-x-1/2 -translate-y-1/2"
+        className="absolute md:w-auto md:h-auto w-[64px] h-[74px] top-[-5rem] lg:top-[-6rem] left-[42%] md:left-[45%] lg:left-[47%] -translate-x-1/2 -translate-y-1/2"
         style={{ transform: 'rotate(319deg)' }}
       />}
       <h1>{splitText.at(0)}<br />{splitText.at(1)}</h1>
